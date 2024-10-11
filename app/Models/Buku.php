ae1f96df1d00ccf124cf81804b29d61440c2ad23
masfajaranugrah <massfajar223@gamil.com>
2024-10-11 19:16:08 +0700

    feat: new

---
+++ app/Models/Buku.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Buku extends Model
+{
+    use HasFactory;
+ 
+    protected $table = 'buku';
+    protected $primaryKey = 'id_buku';
+    
+    protected $fillable = [
+        'judul',
+        'pengarang',
+        'jenis',
+        'jumlah',
+        'tahun_terbit',
+        'tersedia',
+        'foto',
+    ];
+
+    public function peminjaman()
+    {
+        return $this->hasMany(Peminjaman::class, 'id_buku', 'id_buku');
+    }
+ 
+}
---
+++ resources/views/layouts/Footer.blade.php
@@ -0,0 +1,9 @@
+<footer>
+                    <div class="footer clearfix mb-0 text-muted">
+                        <div >
+                            <p>2024 &copy;  </p>
+                        </div>
+                       
+                    </div>
+                  
+                </footer>
